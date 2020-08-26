56bb99aa5eff87cfc0c57d72a58ef9e8affb3aa5
joegturner <59180847+joegturner@users.noreply.github.com>
2020-08-26 01:01:26 -0400

    Updates

---
+++ precache-manifest.162a6592eac59a1df30b92bb731f1362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264b40233c26679867ee861c2f95d3d6",
+    "url": "/news-app-alan-ai/index.html"
+  },
+  {
+    "revision": "91e2732ce16a5ed22ca9",
+    "url": "/news-app-alan-ai/static/css/main.4a1570b1.chunk.css"
+  },
+  {
+    "revision": "3eda62505496af6fef5c",
+    "url": "/news-app-alan-ai/static/js/2.67964de4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/news-app-alan-ai/static/js/2.67964de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e2732ce16a5ed22ca9",
+    "url": "/news-app-alan-ai/static/js/main.33b4bae8.chunk.js"
+  },
+  {
+    "revision": "c49a23b9b212bcac41d8",
+    "url": "/news-app-alan-ai/static/js/runtime-main.1f4c4c83.js"
+  }
+]);
