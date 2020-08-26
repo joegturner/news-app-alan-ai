b8414b6dfb9d6c4c3920ad662c374f451fc251ed
joegturner <59180847+joegturner@users.noreply.github.com>
2020-08-26 00:51:21 -0400

    Updates

---
+++ precache-manifest.4ab6b2388570bb119df912950b8be7f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a571b99726bb395cee8384f36540e25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91e2732ce16a5ed22ca9",
+    "url": "/static/css/main.4a1570b1.chunk.css"
+  },
+  {
+    "revision": "3eda62505496af6fef5c",
+    "url": "/static/js/2.67964de4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.67964de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e2732ce16a5ed22ca9",
+    "url": "/static/js/main.33b4bae8.chunk.js"
+  },
+  {
+    "revision": "84796ccf011447e58ba4",
+    "url": "/static/js/runtime-main.310a1719.js"
+  }
+]);
